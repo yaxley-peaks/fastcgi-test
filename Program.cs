e308c57249772853e2d0f29a57dc78cf2b20e55a
yaxley-peaks <yax@yaxley.in>
2025-03-14 23:20:35 +0530

    works

--- Program.cs
+++ Program.cs
@@ -1,9 +1,31 @@
-﻿namespace fastcgi_service;
+﻿using System.Net;
+using FastCGI;
+
+namespace fastcgi_service;
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var app = new FCGIApplication();
+        app.OnRequestReceived += (sender, request) =>
+        {
+            request.WriteResponseASCII("HTTP/1.1 200 OK\nContent-Type:text/html\n\nThis is some text!\n");
+            request.Close();
+        };
+        
+        Console.WriteLine("Listening...");
+        
+        // Listen on ip 0.0.0.0 to appease docker
+        app.Listen(new IPEndPoint(0L, 19000));
+        while (!app.IsStopping)
+        {
+            if (!app.Process())
+            {
+                Thread.Sleep(1);
+            }
+        }
+        
     }
 }
